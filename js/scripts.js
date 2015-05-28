ac7fbb3912405a9a6544725fe30006239e8c556f
james williams & Alec Arme <student@epicodus.com>
2015-05-28 13:51:56 -0700

    Create initial files. Add spec for single word.

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var wordCount = function(phrase) {
+  return phrase + " " + 1
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('wordCount', function() {
+  it('returns 1 for a single word', function(){
+    expect(wordCount("frank")).to.equal("frank 1")
+  });
+});
