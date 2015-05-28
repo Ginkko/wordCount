295cd1aa915e7ed5f1155278e6a94fe0ee298493
james williams & Alec Arme <student@epicodus.com>
2015-05-28 14:27:36 -0700

    add spec for multiple words and pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 var wordCount = function(phrase) {
-  return phrase + " " + 1
+  var words = phrase.split(" ");
+  var wordStore = {};
+  var output = []
+
+  for (var i = 0; i < words.length; i++) {
+    if (wordStore.hasOwnProperty(words[i])) {
+      wordStore[words[i]] = wordStore[words[i]] + 1
+    } else {
+      wordStore[words[i]] = 1
+    }
+  }
+
+  var finalWords = Object.keys(wordStore)
+
+  for (var i = 0; i <finalWords.length; i++){
+    output.push(finalWords[i], wordStore[words[i]]);
+  }
+  return output.join(" ");
+
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,11 @@ describe('wordCount', function() {
   it('returns 1 for a single word', function(){
     expect(wordCount("frank")).to.equal("frank 1")
   });
+  it('returns 2 for a single word repeated', function() {
+    expect(wordCount("frank frank")).to.equal("frank 2")
+  });
+  it('returns 1s for two words', function() {
+    expect(wordCount("frank ralph")).to.equal("frank 1 ralph 1")
+  });
+
 });
